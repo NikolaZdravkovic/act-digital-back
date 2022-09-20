e668ddb8878922655ec7207a259e4e7a4ee37fb3
Nikola Zdravkovic <zdravko.dzoni@gmail.com>
2022-09-20 12:16:59 +0200

    experties page

---
+++ src/api/experties-page/controllers/experties-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  experties-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::experties-page.experties-page');
---
+++ src/api/experties-page/services/experties-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * experties-page service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::experties-page.experties-page');
