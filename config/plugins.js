e3609d7b8a5587e94e9623e16370fdaa214d8262
Nikola Zdravkovic <zdravko.dzoni@gmail.com>
2022-11-11 22:56:41 +0100

    add editor js for case studies

--- config/plugins.js
+++ config/plugins.js
@@ -15,5 +15,9 @@ module.exports = ({ env }) => ({
         },
       },
     },
+    'editorjs': {
+      enabled: true,
+      resolve: './src/plugins/strapi-plugin-react-editorjs'
+    },
     // ...
   });
